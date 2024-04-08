491323cb06d206412824af25e05ef964d895d6f9
Mh Mitas <mahfuzulmitas@gmail.com>
2024-04-08 10:53:20 +0600

    commit -

--- src/components/pages/properties/AllProperties.jsx
+++ src/components/pages/properties/AllProperties.jsx
@@ -9,11 +9,11 @@ const AllProperties = () => {
 
     return (
         <div>
-            <div>
+            <div className='my-6'>
                 <h3 className='text-3xl font-semibold'>Trending Properties</h3>
                 <p className='text-xl'>Viewed and saved the most over the past 24 hours</p>
             </div>
-            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4'>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto'>
                 {
                     allProperties.map(
                         property => <PropertyCard
--- src/components/pages/properties/PropertyCard.jsx
+++ src/components/pages/properties/PropertyCard.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const PropertyCard = ({ property }) => {
-    const { estate_title, description, image } = property
+    const { estate_title, description, image, segment_name } = property
     return (
-        <div className="card max-w-96 bg-base-200 shadow-xl mb-8">
+        <div className="card max-w-md	 bg-base-200 shadow-xl mb-8">
             <figure><img src={image} alt={estate_title} /></figure>
             <div className="card-body">
+                <div className='badge badge-primary'>{segment_name}</div>
                 <h2 className="card-title">{estate_title}</h2>
                 {/* <p><b>Location:</b> {location}</p> */}
                 {description.length > 84 ? <p>{description.slice(0, 106)}....</p> : <p>{description}</p>}
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <Link to="">
+                        <button className="btn btn-primary">View Property</button>
+                    </Link>
                 </div>
             </div>
         </div>
